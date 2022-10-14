7c3cbec78751925737412e6b37f20d072b31e2ac
Jason <gt07891@greattree.com.tw>
2022-10-14 11:32:03 +0800

    update: page config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,13 +8,19 @@ console.log(mode);
 export default defineUserConfig({
   base: mode === 'development' ? '/' : './',
   lang: 'zh-tw',
-  title: '網頁開發筆記',
+  title: 'Jason\'s Notes',
   head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
 
   theme: defaultTheme({
     lastUpdated: true,
     tip: 'info',
     logo:'images/cat.jpg',
+    repo:'https://github.com/lian0103',
+    docsRepo:'https://github.com/lian0103/notes',
+    docsDir:'/docs',
+    editLinkText:'編輯內容',
+    lastUpdatedText:'最後更新',
+    contributorsText:'編輯者',
     sidebar: [
       {
         text: '首頁',
