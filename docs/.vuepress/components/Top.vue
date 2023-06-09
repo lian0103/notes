4adcdd1b835804dc8557ec020030bb3dff2a34be
Jason Lien 連宗聖 <jasonlien@starlux-airlines.com>
2023-06-09 11:46:16 +0800

    update: for seo

--- docs/.vuepress/components/Top.vue
+++ docs/.vuepress/components/Top.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="my-2">
-      <span class="level-top">置頂</span>  <a class="level-top-text" href="./Stacks/career.html">👨‍🏭 轉職前端陪練 (收費)</a>
-    </div>
-    <div class="my-2">
-      <span class="level-top">置頂</span>  <a class="level-top-text" href="./Stacks/career-job.html">👨‍🏭 Web前端開發 (收費)</a>
-    </div>
+  <div class="my-2">
+    <span class="level-top">置頂</span>
+    <a class="level-top-text" href="./Stacks/career.html"
+      >👨‍🏭 轉職前端陪練/家教 (收費)</a
+    >
+  </div>
+  <div class="my-2">
+    <span class="level-top">置頂</span>
+    <a class="level-top-text" href="./Stacks/career-job.html"
+      >👨‍🏭 Web前端開發 (收費)</a
+    >
+  </div>
 </template>
-
