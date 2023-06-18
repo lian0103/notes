01859d54bafb76c1dbcc24aa0b49a89b409f322c
lian0103 <k1319900103@gmail.com>
2023-06-18 18:52:14 +0800

    fix ads.txt

--- plugins/genAdsTxt.js
+++ plugins/genAdsTxt.js
@@ -2,15 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const outputPath = path.resolve(__dirname, "../docs/.vuepress/dist");
-const fileName = "Ads.txt";
-const fileName2 = "ads.txt";
+const fileName = "ads.txt";
 const fileContent =
   "google.com, pub-1129287891959179, DIRECT, f08c47fec0942fa0";
 
 fs.writeFileSync(`${outputPath}/${fileName}`, fileContent, (err) => {
   console.log("gen Ads.txt err");
-});
-
-fs.writeFileSync(`${outputPath}/${fileName2}`, fileContent, (err) => {
-  console.log("gen ads.txt err");
 });
